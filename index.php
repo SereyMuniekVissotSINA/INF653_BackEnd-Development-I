9472c8894f72aadb4b2b33763ca45dddc73fff5f
SereyMuniekVissot SINA <vissot004@gmail.com>
2026-01-26 13:13:18 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>phpinfo()</h2>
+    <p>This show your PHP environment</p>
+    
+    <?php $name = 'Paul';
+     echo "<h1 >Hello $name</h1>", "<h1> Do you want to juk tnam? </h1>";
+     echo "<pre><h2>Jg dek</h2></pre>" ?>
+</body>
+</html>
